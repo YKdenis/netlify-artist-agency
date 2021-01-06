ccde19771102b602c83a913995d0be91d1789a66
Denis <denis.inghelbrecht@outlook.be>
2021-01-06 12:45:21 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,63 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Artist Agency",
+    description: "Belgian based artist agency",
+    author: "denis",
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/static/img`,
+        name: "images",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/artists`,
+        name: "artists",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-relative-images",
+            options: {
+              staticFolderName: "static",
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1920,
+            },
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-netlify-cms",
+    {
+      resolve: "gatsby-plugin-google-fonts",
+      options: {
+        fonts: ["Roboto", "Oswald"],
+        display: "swap",
+      },
+    },
+  ],
 }
